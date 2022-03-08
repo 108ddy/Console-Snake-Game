2c00f15175861a7d7e2834d8006dee1cd435a364
108ddy <mgbemena900@gmail.com>
2022-03-08 18:11:59 +0200

    Fixed bugs with snake controls and hiding, added game score and snake tail increase, placed into a separate logic part of code from moving_on_field function and a small refactoring code

--- snake.py
+++ snake.py
@@ -7,6 +7,7 @@ import random
 
 frame = width, height = 25, 10
 moving = 1
+score = 0
 
 
 def clear():
@@ -33,9 +34,11 @@ def food_respawn() -> None:
 
 
 def initialize() -> None:
-    global snake_head_position, direction, apple_spawn, apple_position
-
-    snake_head_position = [frame[0] // 4, frame[1] // 2]
+    global snake_position, snake_head, direction, apple_spawn, apple_position
+    
+    snake_position = [
+        [frame[0] // 4, frame[1] // 2],
+    ]
     direction = 'RIGHT'
     apple_spawn = True
     apple_position = food_spawn()
@@ -65,60 +68,101 @@ def draw_field(field: list, frame: tuple) -> None:
     
     for height in range(frame[1] + 2):
         for width in range(frame[0] + 2):
-            if (snake_head_position[1] == height and snake_head_position[0] == width and
-                snake_head_position[1] != 0 and snake_head_position[0] != 0 and
-                snake_head_position[1] != frame[1] + 2 and snake_head_position[0] != frame[0] + 2):
-                char = '■'
-            elif apple_position[1] == height and apple_position[0] == width:
+            if snake_head == [width, height]:
+                char = '◆'
+            elif apple_position == [width, height] and apple_position not in snake_position:
                 char = '@'
             else:
-                char = field[height][width]
+                prints = False
+
+                for tail in snake_position:
+                    if tail == [width, height]:
+                        char = '◆'
+                        prints = True
+                
+                if not prints: 
+                    char = field[height][width]
             
             print(char, end = '')
         
         print()
+    
+    print(f'Score: {score}')
+
+
+def snake_increase() -> None:
+    tail = snake_head[:]
+
+    if len(snake_position) > 1:
+        a, b = snake_position[0], snake_position[1]
+        tail = a[:]
+    
+        if a[1] < b[1]:
+            tail[1] -= 1
+        elif a[0] > b[0]:
+            tail[0] += 1
+        elif a[1] > b[1]:
+            tail[1] += 1
+        elif a[0] < b[0]:
+            tail[0] -= 1
+
+    tail = field_limit(tail, frame)
+    snake_position.insert(0, tail)
 
 
 def controls() -> None:
-    global direction, apple_spawn
-
+    global direction 
+    
     if (keyboard.is_pressed('w') or keyboard.is_pressed('UP')) and direction != 'DOWN':
         direction = 'UP'
     elif (keyboard.is_pressed('d') or keyboard.is_pressed('RIGHT')) and direction != 'LEFT':
         direction = 'RIGHT'
-    if (keyboard.is_pressed('s') or keyboard.is_pressed('DOWN')) and direction != 'UP':
+    elif (keyboard.is_pressed('s') or keyboard.is_pressed('DOWN')) and direction != 'UP':
         direction = 'DOWN'
-    if (keyboard.is_pressed('a') or keyboard.is_pressed('LEFT')) and direction != 'RIGHT':
+    elif (keyboard.is_pressed('a') or keyboard.is_pressed('LEFT')) and direction != 'RIGHT':
         direction = 'LEFT'
 
-    moving_on_field(direction, apple_spawn, frame)
+    moving_on_field(direction, frame)
 
 
-def moving_on_field(direction: str, apple_spawn: bool, frame_size: tuple) -> None:
+def field_limit(position: list, frame_size: tuple) -> list:
+    if position[1] < 1:
+        position[1] = frame_size[1]
+    elif position[0] > frame_size[0]:
+        position[0] = 1
+    elif position[1] > frame_size[1]:
+        position[1] = 1
+    elif position[0] < 1:
+        position[0] = frame_size[0]
+
+    return position
+
+
+def moving_on_field(direction: str, frame_size: tuple) -> None:
+    global snake_head, snake_position, apple_spawn, score
+     
+    snake_head = snake_position[-1][:]
+
     if direction == 'UP':
-        snake_head_position[1] -= moving
+        snake_head[1] -= moving
     elif direction == 'RIGHT':
-        snake_head_position[0] += moving
+        snake_head[0] += moving
     elif direction == 'DOWN':
-        snake_head_position[1] += moving
+        snake_head[1] += moving
     elif direction == 'LEFT':
-        snake_head_position[0] -= moving
+        snake_head[0] -= moving
 
-    if snake_head_position[1] < 0:
-        snake_head_position[1] = frame_size[1]
-    elif snake_head_position[0] > frame_size[0]:
-        snake_head_position[0] = 0
-    elif snake_head_position[1] > frame_size[1]:
-        snake_head_position[1] = 0
-    elif snake_head_position[0] < 0:
-        snake_head_position[0] = frame_size[0]
+    snake_head = field_limit(snake_head, frame_size)
+    del(snake_position[0])
+    snake_position.append(snake_head)
 
-    if apple_position[1] == snake_head_position[1] and apple_position[0] == snake_head_position[0]:
+    if apple_position == snake_head:
         apple_spawn = False
-    
+        score += 1
+        snake_increase()
+
     if not apple_spawn:
         food_respawn()
-
 
 
 def main():
@@ -127,7 +171,7 @@ def main():
     while True:
         controls()
         draw_field(field, frame)
-        sleep(.1)
+        sleep(.2)
 
 
 main()
